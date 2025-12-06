5419583a3b43781ec54d911b2034e085c08686f7
Nikita Sin <74384266+lesleysin@users.noreply.github.com>
2025-12-06 21:01:28 +0200

    Remove package Obsolete packages have been removed from the project, upd Dart SDK version

--- packages/hetu_script_flutter/example/README.md
+++
@@ -1,3 +0,0 @@
-# hetu_script_flutter_example
-
-A example for using Hetu script in Flutter app.
--- packages/hetu_script_flutter/lib/hetu_script_flutter.dart
+++
@@ -1,6 +0,0 @@
-// Hetu Script Flutter
-//
-// Enable Hetu Script in Flutter apps.
-
-export 'src/assets_resource_context.dart';
-export 'src/extension.dart';
